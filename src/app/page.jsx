d0cc13bc9538f14b96a71d2e6caa19036ec33305
Darill <darilinsankamil2132@gmail.com>
2023-10-29 14:59:43 +0700

    fix-Caching

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,5 @@
 import AnimeList from "@/components/Animelist";
 import Header from "@/components/Animelist/Header";
-import RecomendAnime from "@/components/Recomendation";
 import { getData, getNestedAnimeResponse } from "../libs/getData";
 
 const Page = async () => {
@@ -8,7 +7,7 @@ const Page = async () => {
   let rekomenAnime = await getNestedAnimeResponse("recommendations/anime", "entry");
   
   const startIndex = Math.floor(Math.random() * (rekomenAnime.length - 4));
-
+  
   rekomenAnime = {
     data: rekomenAnime.slice(startIndex, startIndex + 4),
   };
--- src/libs/getData.js
+++ src/libs/getData.js
@@ -1,6 +1,6 @@
 export const getData = async (resource, query) => {
     const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`, {cache: 'no-store'}
     );
     const anime = await res.json();
     return anime;
